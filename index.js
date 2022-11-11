742fb665166cbc225a3f623b7f10cc9659c58261
Yadhavan01 <117370292+Yadhavan01@users.noreply.github.com>
2022-11-11 20:32:40 +0530

    change in file location

--- index.js
+++ index.js
@@ -15,35 +15,35 @@ document.addEventListener("keypress", function(event){
 function makeSound(key){
     switch (key) {
         case "C":
-            var music = new Audio("sounds/key-1.mp3")
+            var music = new Audio("key-1.mp3")
             music.play();
             break;
         case "D":
-            var music = new Audio("sounds/key14.mp3")
+            var music = new Audio("key14.mp3")
             music.play();
             break;
         case "E":
-            var music = new Audio("sounds/key15.mp3")
+            var music = new Audio("key15.mp3")
             music.play();
             break;
         case "F":
-            var music = new Audio("sounds/key16.mp3")
+            var music = new Audio("key16.mp3")
             music.play();
             break;
         case "G":
-            var music = new Audio("sounds/key17.mp3")
+            var music = new Audio("key17.mp3")
             music.play();
             break;
         case "A":
-            var music = new Audio("sounds/key18.mp3")
+            var music = new Audio("key18.mp3")
             music.play();
             break;
         case "B":
-            var music = new Audio("sounds/key19.mp3")
+            var music = new Audio("key19.mp3")
             music.play();
             break;
         case "L":
-            var music = new Audio("sounds/key2.mp3")
+            var music = new Audio("key2.mp3")
             music.play();
             break;
 
